1ad6ed988eec77b6bcec5f9185340218f426d8f6
Felipe Barros <felipe.b4rros@gmail.com>
2019-02-15 17:59:36 -0300

    add unittest but not working from bash

--- M2 Exercícios/fizzbuzz.py
+++ M2 Exercícios/fizzbuzz.py
@@ -2,13 +2,14 @@
 # Fizbuzzer
 
 """"Regras do fizz buzz:
-Se posição multipla de 3: fizz
-Se posição for multipla de 5: buzz
-Se posição for multipla de 3 e 5: fizzbuzz
-Para qualquer outra posição: numero
+Se posicao multipla de 3: fizz
+Se posicao for multipla de 5: buzz
+Se posicao for multipla de 3 e 5: fizzbuzz
+Para qualquer outra posicao: numero
 """
 
 from functools import partial
+import unittest
 
 multiple_of = lambda base, num: num % base == 0
 multiple_of_5 = partial(multiple_of, 5)
@@ -25,19 +26,76 @@ def robot(pos):
         say = 'fizz'
     return say
 
+class FizzbuzzTest(unittest.TestCase):
+    def test_say_1_when_1(self):
+        self.assertEqual(robot(1), '1')
+
+    def test_say_2_when_2(self):
+        self.assertEqual(robot(2), '2')
+
+    def test_say_4_when_4(self):
+        self.assertEqual(robot(4), '4')
+
+
+    def test_say_fizz_when_3(self):
+        self.assertEqual(robot(3), 'fizz')
+
+    def test_say_fizz_when_6(self):
+        self.assertEqual(robot(6), 'fizz')
+
+    def test_say_fizz_when_9(self):
+        self.assertEqual(robot(9), 'fizz')
+
+
+    def test_say_buzz_when_5(self):
+        self.assertEqual(robot(5), 'buzz')
+
+    def test_say_buzz_when_10(self):
+        self.assertEqual(robot(10), 'buzz')
+
+    def test_say_buzz_when_20(self):
+        self.assertEqual(robot(20), 'buzz')
+
+
+    def test_say_fizzbuzz_when_15(self):
+        self.assertEqual(robot(15), 'fizzbuzz')
+
+    def test_say_fizzbuzz_when_30(self):
+        self.assertEqual(robot(30), 'fizzbuzz')
+
+    def test_say_fizzbuzz_when_45(self):
+        self.assertEqual(robot(45), 'fizzbuzz')
+
 if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
+    unittest.main()
+"""
+def assert_equal(result, expected):
+    from sys import _getframe
 
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
+    msg = 'fail: Line {} got {} expecting {}'
 
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
+    if not result == expected:
+        current = _getframe() # current pq estamos pegando o frame da funcao;
+        caller = current.f_back # agora, sim pegando o caller da funcao
+        line_no = caller.f_lineno
 
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
+        print(msg.format(line_no, result, expected))
+
+
+if __name__ == '__main__':
+    assert_equal(robot(1), '1')
+    assert_equal(robot(2), '2')
+    assert_equal(robot(4), '4')
+
+    assert_equal(robot(3), 'fizz')
+    assert_equal(robot(6), 'fizz')
+    assert_equal(robot(9), 'fizz')
+
+    assert_equal(robot(5), 'buzz')
+    assert_equal(robot(10), 'buzz')
+    assert_equal(robot(20), 'buzz')
+
+    assert_equal(robot(15), 'fizzbuzz')
+    assert_equal(robot(30), 'fizzbuzz')
+    assert_equal(robot(45), 'fizzbuzz')
+"""
